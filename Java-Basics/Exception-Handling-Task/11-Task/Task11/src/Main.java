037546f81c52232df825c8ec1b08aac9d38a68d3
Ahmed Adel <mandour8808@gmail.com>
2025-11-20 02:23:50 +0200

    Add files via upload

---
+++ Java-Basics/Exception-Handling-Task/11-Task/Task11/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Inside try block");
+            int x = 10 / 0;   // This will cause an ArithmeticException
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Inside catch block: " + e.getMessage());
+        }
+        finally {
+            System.out.println("finally block \"Always Executes\"");
+        }
+
+
+    }
+}
